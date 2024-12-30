a72f789e6303efc8fdd1118ddb8061f587868892
MetrOwO <hyspa.fabio@gmail.com>
2024-12-30 12:39:54 +0100

    Removed sockets, I need to replace that part with HTTP requests

--- Windows/main.c
+++ Windows/main.c
@@ -1,9 +1,6 @@
 #include "tools.h"
 
 int main(int argc, char** argv){
-    printf("%s",argv[1]);
     loggerBody();
-    printf("Finito Logger");
-    initialiseSocketClient(argv[1]);
     return 0;
 }
